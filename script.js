0d2775ada95cf68aa6f037c10a1f3863e9b0c237
Luis Sanchez <luisanchezdh@gmail.com>
2025-07-13 19:42:55 -0400

    added html boilerplate, set up script.js and built add,sub,multiply,divide functions

---
+++ script.js
@@ -0,0 +1,20 @@
+const add = (a,b)=>{
+    return a+b
+}
+
+const subtract = (a,b)=>{
+    return a-b
+}
+
+const multiply = (a,b)=>{
+    return a*b 
+}
+
+const divide = (a,b)=>{
+    return a/b 
+}
+
+console.log(add(1,2)) //3
+console.log(subtract(3,2)) //1
+console.log(multiply(10,10)) //100
+console.log(divide(10,5)) //2
