58297c72521af7cfce36e860c01f05f41c344216
Luis Sanchez <luisanchezdh@gmail.com>
2025-07-13 20:09:00 -0400

    added HTML buttons and operator function

--- script.js
+++ script.js
@@ -14,7 +14,24 @@ const divide = (a,b)=>{
     return a/b 
 }
 
-console.log(add(1,2)) //3
-console.log(subtract(3,2)) //1
-console.log(multiply(10,10)) //100
-console.log(divide(10,5)) //2
+// console.log(add(1,2)) //3
+// console.log(subtract(3,2)) //1
+// console.log(multiply(10,10)) //100
+// console.log(divide(10,5)) //2
+
+let firstNumber = 10
+let secondNumber  = 10
+
+const operate = (operator,a,b) =>{
+    if(operator === "+"){
+        return add(a,b)
+    } else if(operator=== "-"){
+        return subtract(a,b)
+    } else if(operator==="*"){
+        return multiply(a,b)
+    } else if(operator==="/"){
+        return divide(a,b)
+    }
+}
+
+console.log(operate("/",firstNumber,secondNumber))
